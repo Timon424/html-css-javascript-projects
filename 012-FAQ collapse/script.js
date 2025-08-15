6a3a784fe44f496ef0575d748bea669c89a11246
Dima Kazarin <dkazarin424@gmail.com>
2025-08-15 12:47:26 +0300

    Delited other`s files

--- 012-FAQ collapse/script.js
+++
@@ -1,8 +0,0 @@
-const faqContainer = document.querySelector(".faq-container");
-const toggles = faqContainer.querySelectorAll(".faq-toggle");
-
-toggles.forEach((toggle) => {
-  toggle.addEventListener("click", () => {
-    toggle.parentNode.classList.toggle("active");
-  });
-});
