9fcf769fb384a094f5889fc4b8b04d14f46da82c
Dima Kazarin <dkazarin424@gmail.com>
2025-08-17 20:18:32 +0300

    feat: make a 3rd day page

--- 002-progress steps/script.js
+++ 002-progress steps/script.js
@@ -5,18 +5,6 @@ let steps = document.querySelectorAll(".step")
 
 let curentStep = 0
 
-prev.addEventListener("click", () => {
-    if (!prev.disabled) {
-        curentStep--
-        steps[curentStep + 1].classList.remove("active")
-        progress.style.width = curentStep / (steps.length - 1) * 100 + "%"
-        next.disabled = false
-        if (curentStep == 0) {
-            prev.disabled = true
-        }
-    }
-})
-
 next.addEventListener("click", () => {
     if (!next.disabled) {
         curentStep++
@@ -25,6 +13,18 @@ next.addEventListener("click", () => {
         prev.disabled = false
         if (curentStep == steps.length - 1) {
             next.disabled = true
+        }
+    }
+})
+
+prev.addEventListener("click", () => {
+    if (!prev.disabled) {
+        curentStep--
+        steps[curentStep + 1].classList.remove("active")
+        progress.style.width = curentStep / (steps.length - 1) * 100 + "%"
+        next.disabled = false
+        if (curentStep == 0) {
+            prev.disabled = true
         }
     }
 })
